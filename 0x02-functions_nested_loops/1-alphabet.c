c48c45fb0410e46478808508b4683dd221cd5bc2
FabMag23 <magfab23@gmail.com>
2022-12-14 12:46:26 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowecase
+ */
+void print_alphabet(void)
+{
+char let;
+for (let = 'a'; let <= 'z'; let++)
+_putchar(let);
+_putchar('\n');
+}
