84e998cc2735b46d3c7ee846655142bab47ec313
FabMag23 <magfab23@gmail.com>
2022-12-14 09:17:34 -0800

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - main block
  * Description: Get a random number and print the number
  * and if it is positive, negative, or zero
  *Return: 0
  */
-
 int main(void)
 {
 	int n;
