9721a9c1e8e542db074562da43e15f46eba464cb
FabMag23 <magfab23@gmail.com>
2022-12-14 11:57:43 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: printa all possible combinations of two tw-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (j > i)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i * 100 + j != 9899)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
