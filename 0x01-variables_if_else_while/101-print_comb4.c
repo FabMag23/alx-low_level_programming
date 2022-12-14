25432a576efe4312bde78de76ece6e697197e5e2
FabMag23 <magfab23@gmail.com>
2022-12-14 11:31:34 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print combination of two digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int tens;
+int ones;
+for (tens = 0; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(tens + '0');
+putchar(ones + '0');
+if (tens < 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
