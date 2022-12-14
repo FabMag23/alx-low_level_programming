78970aacf63cccdbcc0182256f43cfc3193d0307
FabMag23 <magfab23@gmail.com>
2022-12-14 11:07:17 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print all possible combinations of single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+putchar(num + '0');
+if (num < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
