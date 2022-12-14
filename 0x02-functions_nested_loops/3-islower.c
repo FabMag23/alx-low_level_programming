8d0b4c1aff4f5f559517567bcb64ff4c5ed5f22c
FabMag23 <magfab23@gmail.com>
2022-12-14 13:06:07 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,10 @@
 #include "main.h"
 /**
- * main - check the code.
- * Return: Always 0.
+ *_islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-int main(void)
+int _islower(int c)
 {
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+return (c >= 'a' && c <= 'z');
 }
