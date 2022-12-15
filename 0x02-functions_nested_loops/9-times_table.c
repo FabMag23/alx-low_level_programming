4497fd60399d9562bafab6a5422b531351a18a24
FabMag23 <magfab23@gmail.com>
2022-12-14 17:59:59 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,25 @@
 #include "main.h"
 /**
  * times_table - prints the 9 times table, starting with 0
- * Return: empty output
  */
 void times_table(void)
 {
-int x, y, z, u, d;
-for (x = 0; x < 9; i++)
+int i, j, k;
+for (i = 0; i < 10; i++)
 {
-for (y = 0; y < 10; j++)
+_putchar('0');	
+for (j = 1; j <= 9;  j++)
 {
-z = x * y;
-if (z > 9)
+_putchar(',');
+_putchar(' ');
+k = i * j;
+if (k <= 9)
 {
-u = z % 10;
-d = (z - u) / 10;
-_putchar(44);
-_putchar(32);
-_putchar(d + '0');
-_putchar(u + '0');
+_putchar(' ');
 }
 else
-{
-if (y != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
-}
-_putchar(z + '0');
-}
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
 _putchar('\n');
 }
