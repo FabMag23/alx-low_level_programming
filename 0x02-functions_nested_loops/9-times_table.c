8762a9f41091b95b30685112bc64119cb333c244
FabMag23 <magfab23@gmail.com>
2022-12-14 18:08:06 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * times_table - prints the 9 times table, starting with 0
- */
+*/
 void times_table(void)
 {
 int i, j, k;
 for (i = 0; i < 10; i++)
 {
-    _putchar('0');	
+_putchar('0');	
 for (j = 1; j <= 9;  j++)
 {
 _putchar(',');
